d0d4677579d737ffae78a3dd3a7ce4e77fa783bf
SamuelMccull <Samuel.mcculloch10@outlook.com>
2025-06-14 19:03:25 +0800

    g

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/santa" : "",
+      base: process.argv.includes("dev") ? "" : "/",
     },
   },
 };
